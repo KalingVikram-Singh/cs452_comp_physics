54fb62c20abe9dc981e3468c7dff540ae6fb1ebb
KalingVikram-Singh <97939209+KalingVikram-Singh@users.noreply.github.com>
2024-03-31 19:15:03 +0530

    Add files via upload

---
+++ assignment3/inputfiles_ass3.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Quest 1 a
+Pow_guess_vector=[1,1,1,0]
+A_Powe=[[4,2/3,-4/3,4/3],[2/3,4,0,0],[-4/3,0,6,2],[4/3,0,2,6]]
+
+
+# Quest 1 b
+A_QR=[[4,2/3,-4/3,4/3],[2/3,4,0,0],[-4/3,0,6,2],[4/3,0,2,6]]
+
+# Quest 3
+
+# Observed distribution of students
+observed_chi = [77, 150, 210, 125, 38]
+
+# Quest 4# Data from shipments A and B
+shipment_A = [4.65, 4.84, 4.59, 4.75, 4.63, 4.75, 4.58, 4.82, 4.86, 4.60, 4.77, 4.65]
+shipment_B = [4.75, 4.79, 4.74, 4.74, 4.77, 4.58, 4.81]
+
