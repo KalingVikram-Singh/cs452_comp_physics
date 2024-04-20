299ed127c7943b1307f51ef7bc0a6e233298213e
KalingVikram-Singh <97939209+KalingVikram-Singh@users.noreply.github.com>
2024-04-21 00:39:49 +0530

    Update thermal_monolayer.py

--- Thermal_spike/thermal_monolayer.py
+++ Thermal_spike/thermal_monolayer.py
@@ -3,28 +3,28 @@ import matplotlib.pyplot as plt
 import math
 from mpl_toolkits.mplot3d import Axes3D
 def Ca(T):  #Specific heat capacity for copper lalttice
-    if T < 1358/20:
+    # if T < 1358/20:
         return 0.36 + 8.6*10**(-5)*T + 2.9*10**(-9)*T**(2)
-    if T>1358/20:
-        return 0.5
+    # if T>1358/20:
+    #     return 0.5
     
 def Ce(T):  #Specific heat capacity for electronic lalttice
-    if T < 1795.5/20: 
+    # if T < 1795.5/20: 
         return 12.682 * T
-    if T > 1795.5/20 or T == 1795.5/20:
-        return 227554.754
+    # if T > 1795.5/20 or T == 1795.5/20:
+    #     return 227554.754
          
 def Ka(T):   #Thermal heat conductuvity.
-    if T< 1358/20:
+    # if T< 1358/20:
         return 0.6 + 0.0011*T - 2.6*10**(-7)*T**(2)
-    if T > 1358/20 :
-        return 2.1 
+    # if T > 1358/20 :
+        # return 2.1 
     
 def dKadt(T):   #Derivative of thermal heat conductuvity with respect to temperature.
-    if T< 1358/20:
+    # if T< 1358/20:
         return 0.0011 - 5.2*10**(-7)*T
-    if T > 1358/20 :
-        return 0.1
+    # if T > 1358/20 :
+        # return 0.1
 
 def dKedt(Tl):   #Thermal heat conductuvity for electronic lattifce.
     return -7 * 10**(2)/Tl
@@ -37,11 +37,11 @@ def A(i,k,dr,dt,n,Te):                                              # A term in 
     alpha2 = 100
     t0 = 2.5*10**(-15) 
     r0 = 2.5*10**(-9)
-    Se = 0.00000049
-    T0 = 300
+    Se = 0.00000052
+    T0 = 20
     Be = 1/((2*math.pi**(1.5))*(.84134*(r0**(2))*t0))
     A0 = Be*Se*alpha1*t0/(T0)
-    resul = A0*(np.exp(-alpha2 * (i*dr)-(alpha1**(2) * (n*dt) - 0.75)**2 / 2))
+    resul = A0*(np.exp(-alpha2 * (i*dr)-(alpha1**(2) * (n*dt) - 0.05)**2 / 2))
     # print("A",resul)
     return resul
 
@@ -52,7 +52,7 @@ def B(i,k,dr,dt,n,Ta):                                              # B term in 
     t0 = 2.5*10**(-15)                                            
     tau = 2.5*10**(-13) 
     R0 = 22*10**(-10)
-    Sn = 0.00000090
+    Sn = 0.00000086
     T0 = 20                                                        # Initial temperature, wihtout normalization
     Bn = 9*10**(-11)/((2*math.pi**(1.5))*(.84134*(R0**(2))*tau))
     B0 = Sn*Bn*alpha1*t0/(alpha2*r0*T0)
@@ -67,7 +67,7 @@ def heat_equation_explicit_integration( r, z, t, dt, dr, dz):                   
     Ta_initial = 1                                                                  # Initial temperature for electrons (normalized by room temperature (sample temperature))
     g = 2.3 * 10**(10)                                                              # Electron-phonon coupling constant
     Te = np.zeros((r,z,t))
-    Ta = np.zeros((r, z,t))                                                         # Creating grid for temperature loop
+    Ta = np.zeros((r,z,t))                                                         # Creating grid for temperature loop
     alpha1 = 100                                                                   #Constants of transformation
     alpha2 = 100                                                                    #Constants of transformation
     t0 = 2.5*10**(-14)                                                              # Mean time for perpendicular electrons
@@ -120,9 +120,10 @@ def heat_equation_explicit_integration( r, z, t, dt, dr, dz):                   
 
 
                 # Update temperatures
-                
-                Te[i,j,n+1] = Te[i,j,n] + (Ke(Ta[i,j,n])*dt)  *  (dTe_dr/(i*dr) + d2Te_dr2 + d2Te_dz2) -  (dt/Ce(Te[i,j,n]))* (g*alpha1*t0)*(Te[i,j,n] - Ta[i,j,n]) + (dt/Ce(Te[i,j,n]))*A(i,j,dr,dt,n,Te)
-                Ta[i,j,n+1] = Ta[i,j,n] + (Ka(Ta[i,j,n])*dt)  *  (dTa_dr/(i*dr) + d2Ta_dr2 + d2Ta_dz2) +  (dt/Ca(Ta[i,j,n]))* (g*alpha1*t0)*(Te[i,j,n] - Ta[i,j,n]) + (dt/Ca(Ta[i,j,n]))*B(i,j,dr,dt,n,Ta)
+                # print(d2Ta_dz2,d2Te_dz2,d2Ta_dr2,d2Te_dr2,dTe_dr,dTa_dr)
+                # print((Ke(Ta[i,j,n])*dt)  *  (dTe_dr/(i*dr) + d2Te_dr2 + d2Te_dz2)/Ce(Te[i,j,n]) -  (dt/Ce(Te[i,j,n]))* (g*alpha1*t0)*(Te[i,j,n] - Ta[i,j,n]) + (dt/Ce(Te[i,j,n]))*A(i,j,dr,dt,n,Te),n)
+                Te[i,j,n+1] = Te[i,j,n] + (Ke(Ta[i,j,n])*dt)  *  (dTe_dr/(i*dr) + d2Te_dr2 + d2Te_dz2)/Ce(Te[i,j,n]) -  (dt/Ce(Te[i,j,n]))* (g*alpha1*t0)*(Te[i,j,n] - Ta[i,j,n]) + (dt/Ce(Te[i,j,n]))*A(i,j,dr,dt,n,Te)
+                Ta[i,j,n+1] = Ta[i,j,n] + (Ka(Ta[i,j,n])*dt)  *  (dTa_dr/(i*dr) + d2Ta_dr2 + d2Ta_dz2)/Ca(Ta[i,j,n]) +  (dt/Ca(Ta[i,j,n]))* (g*alpha1*t0)*(Te[i,j,n] - Ta[i,j,n]) + (dt/Ca(Ta[i,j,n]))*B(i,j,dr,dt,n,Ta)
     return Te, Ta
 alpha1 = 100
 t0 = 2.5*10**(-14)
@@ -131,7 +132,7 @@ dz = 10**(-3)                   # finite difference for z direction
 dt = 10**(-9)                # finite difference for time
 r = 25
 z = 25
-t = 600000
+t = 1000
 
 print("The divisions in R, Z and T are:",r,z,t,"\n")
 
@@ -173,11 +174,11 @@ ax.set_zlabel('Temperature (K)')
 plt.show()
 
 # Assuming R=10 and Z=20
-R_index = 10
-Z_index = 16
+R_index = 2
+Z_index = 2
 
 # Extract the temperature values for the specific R and Z
-temp_values = (total_temp[R_index, Z_index, :]*20)
+temp_values = (Te[R_index, Z_index, :]*20)
 
 # Create an array for time
 time = np.arange(0, len(temp_values))
